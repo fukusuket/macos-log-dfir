55685e930c8d1d780255fa9768b1030778c29234
fukusuket <41001169+fukusuket@users.noreply.github.com>
2023-06-19 00:45:02 +0900

    new: add yml read logic

--- src/rulenode.rs
+++ src/rulenode.rs
@@ -1,5 +1,29 @@
-fn node(){
+use std::fs;
+use std::path::Path;
+use yaml_rust::YamlLoader;
 
+fn read_yaml_files(dir: &Path) -> Result<Vec<MyYamlStruct>, Box<dyn std::error::Error>> {
+    let mut yaml_files = vec![];
+    // 指定されたディレクトリ以下のファイルを再帰的に探索
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        // ファイルの拡張子がyamlもしくはymlの場合にのみ処理を行う
+        if let Some(extension) = path.extension() {
+            if extension == "yaml" || extension == "yml" {
+                // YAMLファイルを読み込み、デシリアライズ
+                let file_content = fs::read_to_string(&path)?;
+                let yaml_contents = YamlLoader::load_from_str(&read_content.unwrap());
+                yaml_files.extend(yaml_contents.unwrap().into_iter().map(|yaml_content| {
+                    let filepath = format!("{}", path.as_ref().to_path_buf().display());
+                    (filepath, yaml_content)
+                }));
+            }
+        }
+    }
+
+    Ok(yaml_files)
 }
 
 #[cfg(test)]
